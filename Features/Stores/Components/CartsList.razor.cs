40c9402fea7a66ccd011b17505f9213af4c884ca
hardik4freelancer <hardik4freelancerwork@gmail.com>
2022-02-04 17:37:47 +0530

    store/cart cart listing by store implemented

---
+++ Features/Stores/Components/CartsList.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using WiiTrakClient.DTOs;
+using WiiTrakClient.HttpRepository.Contracts;
+
+namespace WiiTrakClient.Features.Stores.Components
+{
+    public partial class CartsList: ComponentBase
+    {
+        [Parameter]
+        public List<CartDto>? Carts { get; set; } = null;
+
+        [Inject]
+        IDialogService? DialogService { get; set; }
+
+        [Inject]
+        ICartHttpRepository? CartHttpRepository { get; set; }
+
+        private bool _listIsLoading = true;
+
+        protected override void OnParametersSet()
+        {
+            _listIsLoading = false;
+        }
+
+        public async Task OpenCartDetailsDialog(CartDto cart)
+        {
+            var parameters = new DialogParameters();
+            parameters.Add("Cart", cart);
+
+            DialogOptions options = new DialogOptions() { MaxWidth = MaxWidth.Large };
+
+            if (DialogService is null) return;
+
+            var dialog = DialogService.Show<CartDetailsDialog>("Cart Details", parameters);
+            var result = await dialog.Result;
+        }
+    }
+}
