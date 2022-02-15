43cc3b9285a4594daf96b02157b107d34fb2c99d
hardik4freelancer <hardik4freelancerwork@gmail.com>
2022-02-15 19:24:46 +0530

    Cart Route map on cart update dialog

--- Features/Drivers/Components/UpdateCartDialog.razor.cs
+++ Features/Drivers/Components/UpdateCartDialog.razor.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using MudBlazor;
 using WiiTrakClient.DTOs;
 using WiiTrakClient.Enums;
 using WiiTrakClient.Features.Drivers.Models;
+using WiiTrakClient.Shared;
 
 namespace WiiTrakClient.Features.Drivers.Components
 {
     public partial class UpdateCartDialog: ComponentBase
     {
+        [Inject] public IJSRuntime JSRuntime { get; set; }
         [Parameter]
         public CartDto? Cart { get; set; }
 
@@ -19,12 +22,33 @@ namespace WiiTrakClient.Features.Drivers.Components
         public List<RepairIssueDto>? RepairIssues { get; set; }
 
         [CascadingParameter] MudDialogInstance MudDialog { get; set; }
-
+        private IJSObjectReference? _jsModule = null;
+        bool _cartHasGeolocation = true;
+        bool showRoute = false;
         int _selectedStatusInt = 0;
         int _seletedConditionInt = 0;
         string _seletedIssue = "";
         string _otherText = "";
 
+        protected override async Task OnInitializedAsync()
+        {
+            if (_jsModule is null)
+            {
+                _jsModule = await JSRuntime
+                    .InvokeAsync<IJSObjectReference>("import", "./js/CartUpdateMap.js");
+            }
+            var dotNetObjectRef = DotNetObjectReference.Create(this);
+            await _jsModule.InvokeVoidAsync("GetRoutePermission", dotNetObjectRef);
+            StateHasChanged();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (!firstRender) return;
+
+            System.Console.WriteLine($"long: {Cart.TrackingDevice.Longitude}");
+            
+        }
         protected override void OnParametersSet()
         {
             if (Cart is not null)
@@ -44,6 +68,36 @@ namespace WiiTrakClient.Features.Drivers.Components
                         Issue = "Other"
                     });
                 }
+            }
+        }
+        [JSInvokable]
+        public async Task ShowCartRoute(bool routePermission)
+        {
+            System.Console.WriteLine($"show cart Route Permission: {routePermission}");
+
+            showRoute = routePermission;
+            StateHasChanged();
+        }
+        private async Task Showroute()
+        {
+            if (Cart.TrackingDevice is not null && Cart.TrackingDevice.Latitude > 0)
+            {
+                _cartHasGeolocation = true;
+
+                var cartMarker = new CartMarkerInfo
+                {
+                    CartId = Cart.Id,
+                    Lat = Cart.TrackingDevice.Latitude,
+                    Long = Cart.TrackingDevice.Longitude,
+                    PopupContent = "Cart #" + Cart.CartNumber,
+                    Color = "MidnightBlue",
+                    Number = Cart.CartNumber
+                };
+                await _jsModule.InvokeVoidAsync("GetCartRoute", cartMarker);
+            }
+            else
+            {
+                _cartHasGeolocation = false;
             }
         }
 
@@ -71,6 +125,14 @@ namespace WiiTrakClient.Features.Drivers.Components
         void ConditionChangedHandler(int conditionInt)
         {
             _seletedConditionInt = conditionInt;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_jsModule is not null)
+            {
+                await _jsModule.DisposeAsync();
+            }
         }
     }
 }
